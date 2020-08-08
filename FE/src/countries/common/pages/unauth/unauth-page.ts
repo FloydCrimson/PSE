396b0e9e1e122d9c9242b79274d3cd49fb4ceaac
FloydCrimson <36471780+FloydCrimson@users.noreply.github.com>
2020-08-08 16:16:04 +0200

    Removed Slides from UnauthPage

--- FE/src/countries/common/pages/unauth/unauth-page.ts
+++ FE/src/countries/common/pages/unauth/unauth-page.ts
@@ -17,12 +17,6 @@ import * as RoutesIndex from '@countries/routes.index';
 })
 export class UnauthPage {
 
-  signInForm = new FormGroup({
-    email: new FormControl('', [], [this.getEmailValidator.bind(this)]),
-    nickname: new FormControl('', [], [this.getNicknameValidator.bind(this)]),
-    password: new FormControl('', [], [this.getPasswordValidator.bind(this)])
-  });
-
   loginForm = new FormGroup({
     nickname: new FormControl('', [], [this.getNicknameValidator.bind(this)]),
     password: new FormControl('', [], [this.getPasswordValidator.bind(this)])
@@ -46,50 +40,19 @@ export class UnauthPage {
 
   // Validators
 
-  private getEmailValidator(control: AbstractControl): Promise<ValidationErrors | null> | Observable<ValidationErrors | null> {
-    if (!this.signInForm || !this.loginForm) {
-      return of({ message: 'Form not ready.', show: false });
-    }
-    const check = this.signInForm.get('email') === control;
-    const email = control.value;
-    if (email.length === 0) {
-      return of({ message: 'Email too short.', show: email.length > 0 });
-    }
-    return check ? timer(1000).pipe(
-      switchMap(_ => this.backendAuthRest.EmailAvailablePOST({ email })),
-      map(result => {
-        if (result.success) {
-          return result.response.output.email ? null : { message: 'Email not available.', show: true };
-        } else {
-          return { message: 'An error occurred while checking the Email.', show: true };
-        }
-      })
-    ) : of(null);
-  }
-
   private getNicknameValidator(control: AbstractControl): Promise<ValidationErrors | null> | Observable<ValidationErrors | null> {
-    if (!this.signInForm || !this.loginForm) {
+    if (!this.loginForm) {
       return of({ message: 'Form not ready.', show: false });
     }
-    const check = this.signInForm.get('nickname') === control;
     const nickname = control.value;
     if (nickname.length < 6) {
       return of({ message: 'Nickname too short.', show: nickname.length > 0 });
     }
-    return check ? timer(1000).pipe(
-      switchMap(_ => this.backendAuthRest.NicknameAvailablePOST({ nickname })),
-      map(result => {
-        if (result.success) {
-          return result.response.output.nickname ? null : { message: 'Nickname not available.', show: true };
-        } else {
-          return { message: 'An error occurred while checking the Nickname.', show: true };
-        }
-      })
-    ) : of(null);
+    return of(null);
   }
 
   private getPasswordValidator(control: AbstractControl): Promise<ValidationErrors | null> | Observable<ValidationErrors | null> {
-    if (!this.signInForm || !this.loginForm) {
+    if (!this.loginForm) {
       return of({ message: 'Form not ready.', show: false });
     }
     const password = control.value;
@@ -100,15 +63,6 @@ export class UnauthPage {
   }
 
   // Events
-
-  public onSignInClicked(): void {
-    const email = this.signInForm.get('email').value;
-    const nickname = this.signInForm.get('nickname').value;
-    const password = this.signInForm.get('password').value;
-    this.backendAuthRest.SignInPOST({ email, nickname, password }).subscribe((result) => {
-      console.log(result);
-    });
-  }
 
   public onLoginClicked(): void {
     forkJoin(
