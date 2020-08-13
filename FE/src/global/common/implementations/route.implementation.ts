4ac2ee8d22314d0023ce5bba02db81c79cd9224f
FloydCrimson <36471780+FloydCrimson@users.noreply.github.com>
2020-08-13 11:14:51 +0200

    Added undefined to navigation implementations

--- FE/src/global/common/implementations/route.implementation.ts
+++ FE/src/global/common/implementations/route.implementation.ts
@@ -1,6 +1,6 @@
 import { LoadChildren } from '@angular/router';
 
-export interface RouteImplementation<I = {}, Q = {}, R = {}> {
+export interface RouteImplementation<I = undefined, Q = undefined, R = undefined> {
     path: string;
     route?: (keyof R)[];
     loadChildren: LoadChildren;
