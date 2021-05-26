46cbc9f586c2e8f22cd6fd220f57a1ae685a96f2
FloydCrimson <36471780+FloydCrimson@users.noreply.github.com>
2021-05-26 15:35:00 +0200

    Added present and dismiss in PSELoadingService

--- FE-LIB/projects/core/src/services/loading.service.ts
+++ FE-LIB/projects/core/src/services/loading.service.ts
@@ -7,13 +7,14 @@ import { filter } from 'rxjs/operators';
 import { PSEBusyService } from './busy.service';
 import { PSEPipeService } from './pipe.service';
 
-
 @Injectable()
 export class PSELoadingService {
 
     private readonly psePipeService = PSEPipeService.create();
 
     private readonly loaders = new Array<[PSELoadingOptions, number]>();
+
+    private subscriptions?: Subscription;
 
     private element?: HTMLIonLoadingElement;
 
@@ -22,21 +23,27 @@ export class PSELoadingService {
         private readonly loadingController: LoadingController
     ) { }
 
-    public subscribe(tokens: (string | number)[], options?: LoadingOptions): Subscription {
-        const pseOptions = { id: Date.now().toString(), ...options } as PSELoadingOptions;
-        return this.pseBusyService.check(tokens).pipe(
-            filter((busy, index) => busy || index > 0)
-        ).subscribe((busy) => {
-            if (busy) {
-                this.psePipeService.push(this.presentAction.bind(this), pseOptions);
-            } else {
-                this.psePipeService.push(this.dismissAction.bind(this), pseOptions);
-            }
-        });
+    public present(options?: LoadingOptions): Promise<void> {
+        return this.psePipeService.push(this.presentAction.bind(this), this.getPSELoadingOptions(options));
     }
 
-    public dismissAll(): void {
-        this.psePipeService.push(this.dismissAllAction.bind(this));
+    public dismiss(options?: LoadingOptions): Promise<void> {
+        return this.psePipeService.push(this.dismissAction.bind(this), this.getPSELoadingOptions(options));
+    }
+
+    public dismissAll(): Promise<void> {
+        return this.psePipeService.push(this.dismissAllAction.bind(this));
+    }
+
+    public subscribe(tokens: (string | number)[], options?: LoadingOptions): Subscription {
+        const subscription = this.pseBusyService.check(tokens).pipe(
+            filter((busy, index) => busy || (index > 0))
+        ).subscribe((busy) => busy ? this.present(options) : this.dismiss(options));
+        if (!this.subscriptions) {
+            this.subscriptions = new Subscription();
+        }
+        this.subscriptions?.add(subscription);
+        return subscription;
     }
 
     //
@@ -49,7 +56,7 @@ export class PSELoadingService {
             this.loaders.push([pseOptions, 1]);
         }
         if (!this.element) {
-            await this.present();
+            await this.presentElement();
         }
     }
 
@@ -58,7 +65,7 @@ export class PSELoadingService {
         if (index >= 0) {
             if (--this.loaders[index][1] === 0) {
                 if (index === 0) {
-                    await this.dismiss();
+                    await this.dismissElement();
                 }
                 this.loaders.splice(index, 1);
             }
@@ -66,25 +73,32 @@ export class PSELoadingService {
             console.warn('Trying to dismiss a loader not presented.', pseOptions);
         }
         if (!this.element && this.loaders.length > 0) {
-            await this.present();
+            await this.presentElement();
         }
     }
 
     private async dismissAllAction(): Promise<void> {
         if (this.element) {
-            await this.dismiss();
+            await this.dismissElement();
         }
         this.loaders.splice(0);
+        this.subscriptions?.unsubscribe();
+        delete this.subscriptions;
     }
 
-    private async present(): Promise<void> {
+    private async presentElement(): Promise<void> {
         this.element = await this.loadingController.create(this.loaders[0][0]);
         await this.element.present();
     }
 
-    private async dismiss(): Promise<void> {
+    private async dismissElement(): Promise<void> {
         await this.element?.dismiss();
         delete this.element;
+    }
+
+    private getPSELoadingOptions(options?: LoadingOptions): PSELoadingOptions {
+        const id = options?.id || Date.now().toString();
+        return { ...options, id } as PSELoadingOptions;
     }
 
 }
--- FE-LIB/projects/core/src/services/pipe.service.ts
+++ FE-LIB/projects/core/src/services/pipe.service.ts
@@ -3,16 +3,22 @@ import { Injectable } from "@angular/core";
 @Injectable()
 export class PSEPipeService {
 
-    private readonly actions = new Array<[(...args: any[]) => Promise<any>, any[]]>();
+    private readonly actions = new Array<[PSEPipeFunc, PSEPipeParameters<PSEPipeFunc>, PSEPipeResolve<PSEPipeFunc>, PSEPipeReject]>();
 
     constructor() { }
 
-    public async push<F extends (...args: any[]) => Promise<any>>(func: F, ...args: Parameters<F>): Promise<void> {
-        this.actions.push([func, args]);
+    public push<T extends PSEPipeFunc>(func: T, ...args: PSEPipeParameters<T>): Promise<PSEPipeReturnType<T>> {
+        return new Promise<PSEPipeReturnType<T>>(async (resolve, reject) => {
+            this.actions.push([func, args, resolve, reject]);
+            this.exhaust();
+        });
+    }
+
+    private async exhaust(): Promise<void> {
         if (this.actions.length === 1) {
             while (this.actions.length > 0) {
-                const [func, args] = this.actions[0];
-                await func(...args).catch();
+                const [func, args, resolve, reject] = this.actions[0];
+                await func(...args).then((value) => resolve(value)).catch((reason?) => reject(reason));
                 this.actions.splice(0, 1);
             }
         }
@@ -25,3 +31,9 @@ export class PSEPipeService {
     }
 
 }
+
+type PSEPipeFunc = (...args: any) => Promise<any>;
+type PSEPipeResolve<T extends PSEPipeFunc> = (value: PSEPipeReturnType<T> | PromiseLike<PSEPipeReturnType<T>>) => void;
+type PSEPipeReject = (reason?: any) => void;
+type PSEPipeParameters<T extends PSEPipeFunc> = T extends (...args: infer P) => Promise<any> ? P : never;
+type PSEPipeReturnType<T extends PSEPipeFunc> = T extends (...args: any) => Promise<infer R> ? R : any;
